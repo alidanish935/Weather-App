40b0516b790d59688eeef6cba986da5199c8fec6
Danish <alidanish936@gmail.com>
2023-03-24 01:57:39 +0530

    23rd March - initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
+import styled from 'styled-components';
+
 
 function App() {
+  const [city, setCity] = useState("");
+  const [submitStatus, setSubmitStatus] = useState(false);
+  const API_KEY = "2f1529d5c73a063ca5bb0e2b51d3251f"
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  const [post, setPost] = React.useState();
+  const submitFn = () => {
+    setSubmitStatus(!submitStatus)
+  }
+  useEffect(() => {
+    if (city) {
+      axios.get(baseURL).then((response) => {
+        console.log('resp...', response.data)
+        setPost(response.data);
+      })
+    };
+  }, [submitStatus])
+
+ const Button =  styled.button`
+ 
+  color: white;
+  width: 80px;
+  height: 32px;
+  margin-left: 3px;
+  background-color: blue;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+
+ `
+//  const Input = styled.input`
+//  border-radius: 6px;
+//  width: 170px;
+ 
+//  `
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container pb-3">
+      <div class="row">
+        {/* <div class="col"> */}
+
+        <h1 class="alert text-center">Weather App</h1>
+        <div className='flex inputContainer'>
+        <div className="col mb-3">
+            <input type='search'  onChange={(e) => setCity(e.target.value)} placeholder='City' />
+
+          </div>
+          <div className="col mb-3">
+            <input type='search'  placeholder='India' />
+          </div>
+         
+          <div class="col mb-3">
+            <Button  onClick={submitFn} >Submit</Button>
+          </div>
+
+        </div>
+
+        <div class="d-flex justify-content-center " id="weatherwidget">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{post && post.name} , {post && post.sys.country}  Weather</h5>
+              <p>Friday, Dec 09,2022</p>
+              <div id="tempcard">
+                <h6 class="card-subtitle mb2">{post && post.main.temp}°C</h6>
+
+              </div>
+
+              <p>haze</p>
+            </div>
+          </div>
+        </div>
+        <div className='detailComp flex space-between'>
+          <div >
+
+            <div><span>High/Low</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{post && post.main.temp_max}/{post && post.main.temp_min}</span></div>
+            <div>.......................................</div>
+            <div><span>Humadity</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{post && post.main.humidity} %</span></div>
+            <div>.......................................</div>
+            <div><span>Pressure</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{post && post.main.pressure} hpa</span></div>
+            <div>.......................................</div>
+          </div>
+          <div >
+            <div><span>Wind</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{post && post.wind.speed}km/h </span></div>
+            <div>.......................................</div>
+            <div><span>Wind Direction</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{post && post.wind.deg} deg</span></div>
+            <div>.......................................</div>
+            <div><span>sunrise</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>{post && post.sys.sunrise} </span></div>
+            <div>.......................................</div>
+          </div>
+        </div>
+        {/* </div> */}
+      </div>
     </div>
+
   );
 }
 
